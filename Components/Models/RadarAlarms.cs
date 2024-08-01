cec42d296991ca346c189b8d5868f22e95f27e7a
Patrick <seitzpr@gmail.com>
2024-07-31 19:54:56 -0500

    Refactored radar alarm page

--- Components/Models/RadarAlarms.cs
+++ Components/Models/RadarAlarms.cs
@@ -1,24 +1,31 @@
+using System.Text.Json.Serialization;
+
 namespace NOAA_Track.Models
 {
     public class ApiResponse
     {
-        public ContextItem @context { get; set; }
-        public string @id { get; set; }
-        public List<GraphEntry> graph { get; set; }
+        [JsonPropertyName("@context")]
+        public ContextItem context { get; set; }
+        [JsonPropertyName("@id")]
+        public required string id { get; set; }
+        [JsonPropertyName("@graph")]
+        public required List<GraphEntry> Graph { get; set; }
     }
 
     public class ContextItem
     {
-        public string @version { get; set; }
+        [JsonPropertyName("@version")]
+        public required string Version { get; set; }
     }
 
     public class GraphEntry
     {
-        public string @type { get; set; }
-        public string stationId { get; set; }
-        public string status { get; set; }
-        public int activeChannel { get; set; }
-        public string message { get; set; }
-        public string timestamp { get; set; }
+        [JsonPropertyName("@type")]
+        public required string type { get; set; }
+        public required string stationId { get; set; }
+        public required string status { get; set; }
+        public required int activeChannel { get; set; }
+        public required string message { get; set; }
+        public required string timestamp { get; set; }
     }
 }
